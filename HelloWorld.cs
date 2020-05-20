9017ca3aef52f4547065413de2e3465487fef9b8
lwazi@synthesis.co.za <lwazi@synthesis.co.za>
2020-05-20 17:15:01 +0200

    Initial programming commit

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System; //The using keyword is used to include the system namespace in the program
+
+namespace HelloWorldApplication //A namespace is a collection of classes
+{
+    class HelloWorld
+{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+       }
+    }
+}
